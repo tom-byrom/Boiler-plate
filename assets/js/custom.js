d9d2d4acd98b988d693d7202f91b370bea0a9c04
Tom Byrom <34626292+tom-byrom@users.noreply.github.com>
2021-07-08 10:37:31 +0100

    2021 update bootstrap 5.02

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,509 +1,279 @@
-/* Smooth Scroll */
+// let main = document.querySelector('main')
+// let footer = document.querySelector('footer')
+// let carousel = document.querySelector('#carousel')
 
-$(document).ready(function () {
+// var footerHeight = footer.offsetHeight
 
-  $('body').smoothScroll({
-    offset: 0,
-    speed: 400,
-    easing: 'swing',
-    delegateSelector: 'a'
-  });
-});
-
-
-
-/* Fancy Box 
-    
-    $("[data-fancybox]").fancybox({
-		loop: true,
-        keyboard: true,
-        arrows : true,
-        buttons : [
-        'slideShow',
-        'fullScreen',
-        //thumbs',
-        //share',
-        //'download',
-        //'zoom',
-        'close'],
-
-	});
-
-*/
-
-/* Lazy Load Images */
-
-$(function () {
-  $('.lazy').Lazy({
-    effect: 'fadeIn'
-
-  });
-});
-
-
-/* Slick Slider */
-
-$(document).ready(function () {
-  $('.home_search_slider').slick({
-    autoplay: false,
-    lazyLoad: 'ondemand',
-    infinite: false,
-    fade: true,
-    prevArrow: $('.prev-hero'),
-    nextArrow: $('.next-hero')
-  });
-});
-
-
-// Set preferred slidesToShow
-var slidesToShowXL = 4;
-var childElementsXL = $('.home_featured_slick_slider').children().length;
-// Check if we can fulfill the preferred slidesToShow
-if (slidesToShowXL > (childElementsXL - 1)) {
-  // Otherwise, make slidesToShow the number of slides - 1
-  // Has to be -1 otherwise there is nothing to scroll for - all the slides would already be visible
-  slidesToShowXL = (childElementsXL);
-}
-
-// Set preferred slidesToShow
-var slidesToShowL = 3;
-var childElementsL = $('.home_featured_slick_slider').children().length;
-// Check if we can fulfill the preferred slidesToShow
-if (slidesToShowL > (childElementsL - 1)) {
-  // Otherwise, make slidesToShow the number of slides - 1
-  // Has to be -1 otherwise there is nothing to scroll for - all the slides would already be visible
-  slidesToShowL = (childElementsL);
-}
-
-// Set preferred slidesToShow
-var slidesToShowMD = 2;
-var childElementsMD = $('.home_featured_slick_slider').children().length;
-// Check if we can fulfill the preferred slidesToShow
-if (slidesToShowMD > (childElementsMD - 1)) {
-  // Otherwise, make slidesToShow the number of slides - 1
-  // Has to be -1 otherwise there is nothing to scroll for - all the slides would already be visible
-  slidesToShowMD = (childElementsMD);
-}
-
-
-$(document).ready(function () {
-  $('.home_featured_slick_slider').slick({
-    prevArrow: $('.prev-caravan'),
-    nextArrow: $('.next-caravan'),
-    responsive: [
-
-      {
-        breakpoint: 50000,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: slidesToShowXL,
-          slidesToScroll: 4,
-          infinite: false,
-          arrows: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: slidesToShowL,
-          slidesToScroll: 3,
-          infinite: false,
-          arrows: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: slidesToShowMD,
-          slidesToScroll: 2,
-          infinite: false,
-          arrows: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: false,
-          arrows: true,
-          dots: true
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-
-  });
-});
-
-
-$(document).ready(function () {
-  $('.bottom_links_slick').slick({
-    responsive: [
-
-      {
-        breakpoint: 50000,
-        settings: {
-          slidesToShow: 4,
-          arrows: false,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: false,
-          arrows: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: false,
-          arrows: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          infinite: false,
-          dots: true
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-
-  });
-});
-
-
-$(document).ready(function () {
-  $('.caravan_brands_slick').slick({
-
-    responsive: [
-
-      {
-        breakpoint: 50000,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: 5,
-          slidesToScroll: 5,
-          infinite: false,
-          arrows: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: false,
-          arrows: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          lazyLoad: 'ondemand',
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false,
-          infinite: false,
-          dots: true
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-
-  });
-});
-
-
-
-// Unslicks and reslicks responsively 
-$(window).on('resize orientationchange', function () {
-  var mySlider = $('.caravan_brands_slick');
-
-  // without this check, all kinds of weird errors happen, and the slider doesn't really work
-  if (!mySlider.hasClass('slick-initialized')) {
-    mySlider.slick({
-      responsive: [
-
-        {
-          breakpoint: 50000,
-          settings: "unslick"
-
-        },
-        {
-          breakpoint: 1200,
-          settings: {
-            lazyLoad: 'ondemand',
-            slidesToShow: 5,
-            slidesToScroll: 5,
-            infinite: false,
-            arrows: false,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            lazyLoad: 'ondemand',
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: false,
-            arrows: false,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            lazyLoad: 'ondemand',
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            arrows: false,
-            infinite: false,
-            dots: true
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-
-    });
-  }
-});
-
-
-
-$('.caravan_brands_slick').on('destroy', function (event, slick) {
-
-  $('.brands_img').attr("src", $('brands_img').attr("data-lazy"));
-  $('.brands_img').removeAttr("data-lazy");
-  $('.brands_img').removeClass('slick-loading');
-
-});
-
-// Lazy Loading on Hero Slides hack 
-
-$(".home_search_slider").on("lazyLoaded", function (e, slick, image, imageSource) {
-  parentSlide = $(image).parent(".slick-slide");
-  imageSource.src = image.attr("src"); //get source
-  parentSlide.css("background-image", 'url("' + imageSource + '")').addClass("loaded"); //replace with background instead
-  image.remove(); // remove source
-});
-
-
-
-// Caravan Gallery 
-
-$('.caravan_gallery_large').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.caravan_gallery_small'
-});
-$('.caravan_gallery_small').slick({
-  prevArrow: $('.prev-caravan-small'),
-  nextArrow: $('.next-caravan-small'),
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.caravan_gallery_large',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
-
-
-
-
-
-// Google maps
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 51.236447,
-      lng: -3.002586
-    },
-    zoom: 12,
-    disableDefaultUI: true,
-    scrollwheel: false,
-    zoomControl: true,
-    styles: [{
-        "featureType": "administrative",
-        "elementType": "all",
-        "stylers": [{
-          "saturation": "-100"
-        }]
-      },
-      {
-        "featureType": "administrative.province",
-        "elementType": "all",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      },
-      {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [{
-            "saturation": -100
-          },
-          {
-            "lightness": 65
-          },
-          {
-            "visibility": "on"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [{
-            "saturation": -100
-          },
-          {
-            "lightness": "50"
-          },
-          {
-            "visibility": "simplified"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [{
-          "saturation": "-100"
-        }]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [{
-          "visibility": "simplified"
-        }]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "all",
-        "stylers": [{
-          "lightness": "30"
-        }]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "all",
-        "stylers": [{
-          "lightness": "40"
-        }]
-      },
-      {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [{
-            "saturation": -100
-          },
-          {
-            "visibility": "simplified"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [{
-            "hue": "#ffff00"
-          },
-          {
-            "lightness": -25
-          },
-          {
-            "saturation": -97
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels",
-        "stylers": [{
-            "lightness": -25
-          },
-          {
-            "saturation": -100
-          }
-        ]
-      }
-    ]
-  });
-
-  var image =
-    '/assets/images/map-marker.svg';
-  var caravanmarker = new google.maps.Marker({
-    position: {
-      lat: 51.236447,
-      lng: -2.952681
-    },
-    map: map,
-    icon: image
+// init rellax on desktop
+if (window.innerWidth >= 992) {
+  var rellax = new Rellax('.rellax', {
+    center:true
   });
 }
 
-
-// Dropdown for shar links
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function reportWindowSize() {
+  rellax.refresh()
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+window.onresize = reportWindowSize
+
+
+
+
+// Calculate screen height and create a css variable on root
+document.querySelector(':root').style.setProperty('--vh', window.innerHeight / 100 + 'px');
+// document.querySelector(':root').style.setProperty('--footer', footer.offsetHeight + 'px');
+
+// main.style.marginBottom = footerHeight + 'px';
+
+window.addEventListener('resize', () => {
+  document.querySelector(':root').style.setProperty('--vh', window.innerHeight / 100 + 'px');
+  // document.querySelector(':root').style.setProperty('--footer', footer.offsetHeight + 'px');
+  // main.style.marginBottom = footerHeight + 'px';
+
+
+})
+
+
+// Hightlight active menu item
+function menuActive() {
+  var current = location.pathname.split('/')[1];
+  if (current === "") return;
+  var menuItems = document.querySelectorAll('.nav-link');
+  for (var i = 0, len = menuItems.length; i < len; i++) {
+    if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+      menuItems[i].classList.add("active");
     }
   }
 }
+
+menuActive()
+
+
+// Get the height of the client
+var clientHeight = window.innerHeight
+
+
+
+// Toggle the stuidos nav
+var scrollPosition = window.scrollY;
+let studioNav = document.querySelector('.studio_nav');
+
+if (studioNav) {
+  window.addEventListener('scroll', function() {
+
+    scrollPosition = window.scrollY;
+
+    if (scrollPosition >= clientHeight / 2) {
+      studioNav.classList.remove('hide_nav');
+    } else {
+      studioNav.classList.add('hide_nav');
+    }
+  });
+}
+
+
+
+// Anchor links nav
+let mainNavLinks = document.querySelectorAll(".studio_nav a");
+let clientHeightHalf = clientHeight / 2
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY + clientHeightHalf;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
+
+
+// Toggle sticky header
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header')
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+
+// Smooth Scroll
+const links = document.querySelectorAll(".smoothScroll");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop - 60,
+    behavior: "smooth"
+  });
+}
+
+
+// Swiper inits
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 16,
+  direction: 'vertical',
+  mousewheel: true,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    hide: true,
+  },
+});
+var gallerySide = new Swiper('.gallery-side', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
+
+
+let swiperFranchises = new Swiper('.swiper-container-franchises', {
+  slidesPerView: 1,
+  grabCursor: true,
+  spaceBetween: 30,
+  // Disable preloading of all images
+  preloadImages: false,
+  // Enable lazy loading
+  lazy: {
+    loadPrevNext: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next-franchises',
+    prevEl: '.swiper-button-prev-franchises',
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    640: {
+      slidesPerView: 1.05,
+      spaceBetween: 15,
+      navigation: false,
+    }
+  }
+});
+
+
+
+
+
+// Hide text on horizontal scroll
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    swiperFranchises.on('fromEdge', function () {
+      let textFadeOut = document.getElementById('fadeOnSlide')
+      textFadeOut.classList.add('opacity_0')
+    });
+
+    swiperFranchises.on('reachBeginning', function () {
+      let textFadeOut = document.getElementById('fadeOnSlide')
+      textFadeOut.classList.remove('opacity_0')
+    });
+  } else {
+
+  }
+}
+
+var x = window.matchMedia("(min-width: 992px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+  window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove = preventDefault; // mobile
+  document.onkeydown = preventDefaultForScrollKeys;
+}
+
+
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY >= footerHeight) {
+//         carousel.style.zIndex = "-2"
+//     }
+//     if (window.scrollY <= footerHeight) {
+//       carousel.style.zIndex = "unset"
+//   }
+// })
+
+
+// if(window.location.href.indexOf('/projects') != -1 ){
+
+//     function openDraw() {
+//         let drawOpen = document.querySelectorAll('.open-drawer')
+//         for (draw of drawOpen) {
+//             draw.addEventListener('click', function(){
+//                 const target = draw.closest('.drawer');
+//                 if(target.classList.contains('open')){
+//                     target.classList.remove('open');
+//                 } else {
+//                     target.classList.add('open');
+//                 }
+//             });
+//         }
+//     }
+
+//     openDraw()
+
+//     let drawer = document.querySelector('#drawer')
+//     let carousel = document.querySelector('#carousel')
+
+//     function drawOpen() {
+//         if(drawer.classList.contains('open')){
+//             target.classList.remove('open');
+//         } else {
+//             drawer.classList.add('open');
+//         }
+//     }
+
+//     function openDrawWheel() {
+//         carousel.addEventListener('touchstart', drawOpen);
+//         carousel.addEventListener('wheel', drawOpen);
+//     }
+
+//     openDrawWheel()
+
+// }
+
+
+// Remove preload
+
+// window.onload = (event) => {
+//   var body = document.querySelector('body')
+//   body.classList.remove('preload')
+// };
